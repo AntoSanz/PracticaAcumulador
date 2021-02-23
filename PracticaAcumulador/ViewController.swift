aa9ab8aaf9c500db4967691667343f865af73cb7
AntoSanz <aspans@gmail.com>
2021-02-23 12:51:38 +0100

    Finished

--- PracticaAcumulador/ViewController.swift
+++ PracticaAcumulador/ViewController.swift
@@ -2,18 +2,67 @@
 //  ViewController.swift
 //  PracticaAcumulador
 //
-//  Created by Mañanas on 23/2/21.
+//  Created by AntoSanz on 23/2/21.
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
-
+    var symbol = 0
+    @IBOutlet weak var outResult: UILabel!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var btnSubtract: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setStyle(sender: btnAdd)
     }
-
-
+    
+    
+    @IBAction func add(_ sender: UIButton) {
+        var result:Int = Int(outResult.text!)!
+        if symbol == 0 {
+            result += Int(sender.tag)
+        } else {
+            result -= Int(sender.tag)
+            if result < 0 {
+                result = 0
+            }
+        }
+        outResult.text = String(result)
+    }
+    @IBAction func reset(_ sender: UIButton) {
+        outResult.text = "0"
+        removeStyle()
+        setStyle(sender: btnAdd)
+    }
+    
+    @IBAction func selectSymbol(_ sender: UIButton) {
+        removeStyle()
+        setStyle(sender: sender)
+    
+        if sender.tag == 1 {
+            symbol = 0
+        } else if sender.tag == 2{
+            symbol = 1
+        } else {
+            symbol = 0
+        }
+    }
+    func setStyle(sender: UIButton) {
+        btnAdd.layer.cornerRadius = 8.0
+        btnSubtract.layer.cornerRadius = 8.0
+        
+        sender.layer.borderWidth = 2.0
+        sender.layer.borderColor = UIColor.systemBlue.cgColor
+    }
+    func removeStyle(){
+        btnAdd.layer.borderColor = UIColor.white.cgColor
+        btnSubtract.layer.borderColor = UIColor.white.cgColor
+        
+        btnAdd.layer.borderWidth = 0
+        btnSubtract.layer.borderWidth = 0
+    }
 }
 
