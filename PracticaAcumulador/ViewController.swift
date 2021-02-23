424981e4f49a752b67a24af136b221e8226ccb25
AntoSanz <aspans@gmail.com>
2021-02-23 11:36:58 +0100

    Initial Commit

---
+++ PracticaAcumulador/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaAcumulador
+//
+//  Created by Mañanas on 23/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
